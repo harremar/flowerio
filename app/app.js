e9c50444afeb2bd4ded8ee04f49a61ca832d753a
harremar <78804575+harremar@users.noreply.github.com>
2022-03-02 21:56:21 -0500

    adding add to cart and cart circle count

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const link = document.querySelector(".links a");
+const additembtn = document.getElementsByClassName("additembtn");
+
+const circle = document.querySelector(".circleCount");
 
 //when hamburger is clicked activate mobileMenu function
 hamburger.addEventListener("click", mobileMenu);
@@ -11,4 +14,13 @@ function mobileMenu() {
   hamburger.classList.toggle("active");
   // opens up navigation links
   navMenu.classList.toggle("active");
+}
+
+let cartNumbers = 0;
+function addCart() {
+  alert("Item has been added to cart");
+  cartNumbers = cartNumbers + 1;
+  console.log(cartNumbers);
+  circle.style.display = "flex";
+  circle.innerHTML = cartNumbers;
 }
